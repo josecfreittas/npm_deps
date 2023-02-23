9af37f68e8c83dcdb65a054d3ccea241f4dee66d
Camelo <josecfreittas@outlook.com>
2023-02-23 15:18:14 -0300

    build: depend on inets and ssl

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule NpmDeps.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :inets, :ssl]
     ]
   end
 
